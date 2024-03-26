d654c45073cf0f4505f7e52ad6b1581c637354d7
Yoan <mrts.yoan@gmail.com>
2024-03-26 13:18:56 +0100

    First page and templating

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+    #[Route('/', name: 'home')]
+    function index(Request $request) : Response {
+        return $this->render('home/index.html.twig');
     }
 }
